7c69eeb67779e7b6f4a805d83b6ea45ed1b2aef5
Matthew Thompson <matthew.thompson@nasa.gov>
2025-03-13 07:06:20 -0400

    Commit latest updates

---
+++ Core/GEOSpyD/24.11.3-0/3.13.lua
@@ -0,0 +1,39 @@
+--[[ File for GEOSpyD
+
+Installed using the GEOSpyD installer script on github:
+
+git@github.com:GMAO-SI-Team/GEOSpyD.git
+
+and I ran
+   
+ml gcc/12.1.0
+./install_miniforge.bash --python_version 3.13 --miniforge_version 24.11.3-0 --ffnet-hack --prefix /ford1/share/gmao_SIteam/GEOSpyD |& tee install.mini24.11.3-0.py3.13.2025-03-03.log
+
+NOTE 1: I have to use the --ffnet-hack to have the script install from a hacked fork of ffnet. For some reason, bucy will *not* install ffnet
+        correctly even though all other systems do. Note that this assumes the FC is just 'gfortran'
+NOTE 2: You'll need to set the date below!
+
+--]]
+
+family("Python")
+
+local installdir = "/ford1/share/gmao_SIteam/GEOSpyD"
+
+local miniforge_version = "24.11.3-0"
+local python_version = "3.13"
+local geospyd_version = "py" .. python_version
+local builddate = "2025-03-03"
+
+local pathdir = pathJoin(miniforge_version,builddate,'envs',geospyd_version)
+local pkgdir = pathJoin(installdir,pathdir)
+
+-- Setup Modulepath for packages built by this compiler
+local mroot = "/ford1/share/gmao_SIteam/lmodulefiles"
+local mdir  = pathJoin(mroot,"GEOSpyD",miniforge_version,geospyd_version)
+prepend_path("MODULEPATH", mdir)
+
+unsetenv("ESMA_F2PY")
+
+prepend_path("PATH",pathJoin(pkgdir,"bin"))
+prepend_path("PROJ_LIB",pathJoin(pkgdir,"share/proj"))
+prepend_path("MANPATH",pathJoin(pkgdir,"share/man"))
