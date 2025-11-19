fa2e928a446de87084771b00159247ed1f2160b5
Matthew Thompson <matthew.thompson@nasa.gov>
2025-11-19 07:31:48 -0500

    latest updates

---
+++ Core/ifx/2025.3.lua
@@ -0,0 +1,93 @@
+--[[ lua modulefile for ifx 2025.3
+
+NOTENOTENOTE: See below for needed patch!
+
+NOTE: It looks like the Base installer is no longer needed!
+
+This was installed using the intel-oneapi-hpc-toolkit-2025.3.0.381_offline.sh installer:
+
+wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/66021d90-934d-41f4-bedf-b8c00bbe98bc/intel-oneapi-hpc-toolkit-2025.3.0.381_offline.sh
+
+After running `bash intel-oneapi-hpc-toolkit-2025.3.0.381_offline.sh` the GUI came up and
+a custom install was selected. In that I changed the install directory to:
+
+/ford1/share/gmao_SIteam/intel/oneapi/2025
+
+NOTE: This is down in the lower left corner of the GUI. Not too obvious.
+
+For some reason, you can't just go up to "oneapi" anymore. It complains about a
+non-empty directory.
+
+and selected these as the components:
+
+Intel oneAPI DPC++/C++ Compiler
+Intel Fortran Compiler
+Intel Distribution for GDB
+Intel oneAPI Math Kernel Library
+Intel Integrated Performance Primitives
+Intel MPI Library
+Intel oneAPI DPC++ Library
+Intel DPC++ Compatability Tool
+Intel oneAPI Threading Building Blocks
+
+PATCH NEEDED!
+
+Per Dom Heinzeller, the following patch is needed to make the ifx compiler
+work. Without it we can't make static libraries.
+
+You need to run:
+
+patchelf --add-needed libm.so.6 /ford1/share/gmao_SIteam/intel/oneapi/2025/compiler/2025.3/lib/libimf.so
+
+--]]
+
+family("Compiler")
+
+local compilername = "ifx"
+local version = "2025.3"
+local oneapidir = "/ford1/share/gmao_SIteam/intel/oneapi/2025"
+local compilerdir = pathJoin(oneapidir,"compiler",version)
+local mkldir = pathJoin(oneapidir,"mkl",version)
+
+-- Setup Modulepath for packages built by this compiler
+local mroot = "/ford1/share/gmao_SIteam/lmodulefiles"
+local mdir  = pathJoin(mroot,"Compiler/ifx-2025.3")
+prepend_path("MODULEPATH", mdir)
+
+setenv("CC",pathJoin(compilerdir,"bin","icx"))
+setenv("CCC",pathJoin(compilerdir,"bin","icpx"))
+setenv("CXX",pathJoin(compilerdir,"bin","icpx"))
+setenv("FC",pathJoin(compilerdir,"bin","ifx"))
+setenv("F90",pathJoin(compilerdir,"bin","ifx"))
+setenv("F77",pathJoin(compilerdir,"bin","ifx"))
+
+setenv("I_MPI_CC",pathJoin(compilerdir,"bin","icx"))
+setenv("I_MPI_CXX",pathJoin(compilerdir,"bin","icpx"))
+setenv("I_MPI_F90",pathJoin(compilerdir,"bin","ifx"))
+
+-- We also need a backing gcc compiler
+-- local gccdir = '/ford1/local/gcc/gcc-12.1.0'
+local gccdir = '/ford1/share/gmao_SIteam/gcc/gcc-13.3.0/'
+prepend_path("PATH",pathJoin(gccdir,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(gccdir,"lib64"))
+prepend_path("CPATH",pathJoin(gccdir,"include"))
+
+-- ifx specific
+
+prepend_path("PATH",pathJoin(compilerdir,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(compilerdir,"lib"))
+prepend_path("LIBRARY_PATH",pathJoin(compilerdir,"lib"))
+prepend_path("NLSPATH",pathJoin(compilerdir,"lib/compiler/locale/en_US/%N"))
+prepend_path("PKG_CONFIG_PATH",pathJoin(compilerdir,"lib/pkgconfig"))
+prepend_path("MANPATH",pathJoin(compilerdir,"share/man"))
+
+-- mkl specific
+
+setenv("MKLROOT",mkldir)
+setenv("MKLPATH",pathJoin(mkldir,"lib"))
+prepend_path("INCLUDE",pathJoin(mkldir,"include"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(mkldir,"lib"))
+prepend_path("LIBRARY_PATH",pathJoin(mkldir,"lib"))
+prepend_path("CPATH",pathJoin(mkldir,"include"))
+
+
---
+++ MPI/ifx-2025.2/intelmpi-2021.16/Baselibs/8.19.0.lua
@@ -0,0 +1,42 @@
+--[[
+
+This was cloned with:
+
+  git clone -j 4 --recurse-submodules -b v8.19.0 https://github.com/GEOS-ESM/ESMA-Baselibs.git ESMA-Baselibs-8.19.0/src
+
+I then updated yafyaml, pflogger and pfunit to the bugfix/ifx-2025.2-support branch
+
+This was built using:
+
+  ml GEOSpyD/24.11.3-0/3.12 ifx/2025.2 intelmpi/2021.16
+
+  make -j6 install ESMF_COMM=intelmpi CONFIG_SETUP='ifx_2025.2-intelmpi_2021.16' |& tee makeinstall.ifx_2025.2-intelmpi_2021.16.log
+
+--]]
+
+family("Baselibs")
+
+local compilername = "ifx-2025.2"
+local mpiname = "intelmpi-2021.16"
+
+local configsetup = "ifx_2025.2-intelmpi_2021.16"
+
+local version = myModuleVersion()
+local baselibdir = "/ford1/share/gmao_SIteam/Baselibs/ESMA-Baselibs-" .. version
+local pkgdir = pathJoin(baselibdir,"x86_64-pc-linux-gnu",configsetup)
+
+-- Setup Modulepath for packages built by this MPI stack
+local mroot = "/ford1/share/gmao_SIteam/lmodulefiles"
+local mdir = pathJoin(mroot,"Baselibs",compilername,mpiname)
+prepend_path("MODULEPATH", mdir)
+
+setenv("BASEDIR",pkgdir)
+setenv("basedir",pkgdir)
+setenv("BASEBIN",pathJoin(pkgdir,"Linux/bin"))
+
+prepend_path("PATH",pathJoin(pkgdir,"Linux/bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"Linux/lib"))
+prepend_path("DYLD_LIBRARY_PATH",pathJoin(pkgdir,"Linux/lib"))
+prepend_path("MANPATH",pathJoin(pkgdir,"Linux/share/man"))
+
+setenv("UDUNITS2_XML_PATH",pathJoin(pkgdir,"Linux/share/udunits/udunits2.xml"))
