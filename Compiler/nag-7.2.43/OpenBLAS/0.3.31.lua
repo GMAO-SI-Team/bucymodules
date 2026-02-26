873c23a1d6ff5ac1a9b2992c491ac466eea81675
Matthew Thompson <matthew.thompson@nasa.gov>
2026-02-26 14:52:53 -0500

    Add nag 7.2.43

---
+++ Compiler/nag-7.2.43/OpenBLAS/0.3.31.lua
@@ -0,0 +1,27 @@
+--[[ Ugly Lua module for OpenBLAS
+
+Built as:
+
+ml nag/7.2.43
+
+cmake -B build-nag-7.2.43 -S . --install-prefix=/ford1/share/gmao_SIteam/OpenBLAS/nag-7.2.43/0.3.31 |& tee cmake.nag-7.2.43.log
+cmake --build build-nag-7.2.43 -j8 |& tee build.nag-7.2.43.log
+cmake --install build-nag-7.2.43 |& tee install.nag-7.2.43.log
+
+--]]
+
+
+local name = "OpenBLAS"
+local compiler = "nag-7.2.43"
+local version = "0.3.31"
+local installdir = "/ford1/share/gmao_SIteam"
+local pkgdir = pathJoin(installdir,name,compiler,version)
+
+whatis([===[loads the OpenBLAS 0.3.31 environment]===])
+
+setenv("BLAS_ROOT",pkgdir)
+setenv("LAPACK_ROOT",pkgdir)
+
+prepend_path{"INCLUDE",pathJoin(pkgdir,"include")}
+prepend_path{"CMAKE_PREFIX_PATH",pathJoin(pkgdir,"lib64/cmake")}
+prepend_path{"PKG_CONFIG_PATH",pathJoin(pkgdir,"lib64/pkgconfig")}
---
+++ Compiler/nag-7.2.43/openmpi/5.0.3-GillesNAGPatch.lua
@@ -0,0 +1,43 @@
+--[[
+
+This is based off of a tarball from @ggouaillardet from https://github.com/open-mpi/ompi/issues/12413#issuecomment-2041302131
+
+Built with:
+
+module load nag/7.2.43
+
+mkdir build-nag-7.2.43 && cd build-nag-7.2.43
+../configure --disable-wrapper-rpath --disable-wrapper-runpath \
+   CC=gcc CXX=g++ FC=nagfor \
+   --with-pmix=internal --with-hwloc=internal --with-libevent=internal \
+   --prefix=/ford1/share/gmao_SIteam/MPI/openmpi/5.0.3-GillesNAGPatch/nag-7.2.43 |& tee configure.nag-7.2.43.log
+
+mv config.log config.nag-7.2.43.log
+make -j4 |& tee make.nag-7.2.43.log
+make install |& tee makeinstall.nag-7.2.43.log
+make check |& tee makecheck.nag-7.2.43.log
+
+--]]
+
+family("MPI")
+prereq("nag/7.2.43")
+
+local compilername = "nag-7.2.43"
+local version = "5.0.3-GillesNAGPatch"
+local siteamdir = "/ford1/share/gmao_SIteam"
+local pkgdir = pathJoin(siteamdir,"MPI","openmpi",version,compilername)
+
+-- Setup Modulepath for packages built by this MPI stack
+local mroot = "/ford1/share/gmao_SIteam/lmodulefiles"
+local mdir = pathJoin(mroot,"MPI/nag-7.2.43",("openmpi-"..version))
+prepend_path("MODULEPATH", mdir)
+
+setenv("OPENMPI",pkgdir)
+setenv("MPIHOME",pkgdir)
+setenv("MPI_HOME",pkgdir)
+
+prepend_path("PATH",pathJoin(pkgdir,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("INCLUDE",pathJoin(pkgdir,"include"))
+prepend_path("MANPATH",pathJoin(pkgdir,"share/man"))
